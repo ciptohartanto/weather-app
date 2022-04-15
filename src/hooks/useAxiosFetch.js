52a891d6992fe66c671849bae8a55cfcef9003be
cipto.hartanto <cipto.hartanto@yahoo.com>
2022-04-16 04:07:22 +0800

    ✨ create useAxiosFetch

---
+++ src/hooks/useAxiosFetch.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useAxiosFetch = () => {
+  const BASE_URL = `https://api.openweathermap.org/data/2.5/`;
+
+  const [weatherData, setweatherData] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const source = axios.CancelToken.source();
+  let isMounted = true;
+  const getNewWeatherData = async (q) => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.get(`${BASE_URL}/weather?`, {
+        params: {
+          q,
+          appid: process.env.REACT_APP_WEATHER_KEY,
+        },
+        cancelToken: source.token,
+      });
+      setweatherData(data);
+      console.log('get');
+    } catch (err) {
+      setFetchError(err.message);
+    } finally {
+      setIsLoading(false);
+      console.log('final');
+    }
+  };
+
+  useEffect(() => {
+    if (isMounted) {
+      getNewWeatherData('taipei');
+    }
+
+    const cleanup = () => {
+      isMounted = false;
+      source.cancel();
+    };
+
+    return () => cleanup;
+  }, []);
+
+  return { weatherData, fetchError, isLoading, getNewWeatherData };
+};
+
+export default useAxiosFetch;
