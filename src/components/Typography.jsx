2a6eb9dbb1eaf817ea23ae9a3b21122f99ccde77
cipto.hartanto <cipto.hartanto@yahoo.com>
2022-04-15 22:39:09 +0800

    ✨ draft: Typography component

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -1,5 +1,13 @@
 import { useMemo } from 'react';
 
+export const TYPOGRAPHY_STYLES = {
+  SUBTITLE: 'subtitle',
+  TITLE: 'title',
+  CAPTION: 'caption',
+  SUBCAPTION: 'subcaption',
+  TEXT: 'text',
+};
+
 const Typography = ({ styleType, text }) => {
   // 1. h1, title -> 40px
   // 2. h2, subtitle -> 24px
@@ -7,10 +15,12 @@ const Typography = ({ styleType, text }) => {
   // 4. h4, subcaption -> 14px
   // 5. h5, text -> 12px
   const typoUI = useMemo(() => {
-    if (styleType === 'h1') return <h1>{text}</h1>;
+    if (styleType === TYPOGRAPHY_STYLES.TITLE) return <h1>{text}</h1>;
+    if (styleType === TYPOGRAPHY_STYLES.SUBTITLE) return <h2>{text}</h2>;
+    if (styleType === TYPOGRAPHY_STYLES.CAPTION) return <h3>{text}</h3>;
+    if (styleType === TYPOGRAPHY_STYLES.SUBCAPTION) return <h4>{text}</h4>;
     return <h5>{text}</h5>;
   }, [styleType]);
-  // styles:
 
   return typoUI;
 };
