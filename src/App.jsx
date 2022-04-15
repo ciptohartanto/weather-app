6aa0cc651b6f4c636ac6929f9302815277cf2112
cipto.hartanto <cipto.hartanto@yahoo.com>
2022-04-15 22:39:25 +0800

    ✨ draft: main markup with Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import Typography from './components/Typography';
+import Typography, { TYPOGRAPHY_STYLES } from './components/Typography';
 
 function App() {
-  return <Typography styleType="h1" text="helloooo!" />;
+  return (
+    <div className="wrapper">
+      <div className="app-top">
+        <div className="app-text__fulldate">
+          <Typography
+            styleType={TYPOGRAPHY_STYLES.SUBCAPTION}
+            text="Monday, 27th April"
+          />
+        </div>
+        <div className="app-text__time">
+          <Typography styleType={TYPOGRAPHY_STYLES.CAPTION} text="6:27am" />
+        </div>
+        <div className="app-text__city">
+          <Typography styleType={TYPOGRAPHY_STYLES.SUBCAPTION} text="London" />
+        </div>
+      </div>
+      <div className="app-middle">
+        <div className="app-text__fulldate">{/* weather icon */}</div>
+        <div className="app-text__temp">
+          <Typography styleType={TYPOGRAPHY_STYLES.CAPTION} text="10*" />
+        </div>
+      </div>
+      <div className="app-bottom">
+        <div className="app-text__date">{/* weather icon */}</div>
+        <div className="app-text__temp">
+          <Typography styleType={TYPOGRAPHY_STYLES.CAPTION} text="10*" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
