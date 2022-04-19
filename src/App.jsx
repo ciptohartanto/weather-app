db7da5ca3695325884627fd604b5571feadea82d
cipto.hartanto <cipto.hartanto@yahoo.com>
2022-04-19 22:44:14 +0800

    🏗️ work on markup draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Typography, { TYPOGRAPHY_STYLES } from './components/Typography';
 import useAxiosFetch from './hooks/useAxiosFetch';
 
 function App() {
   const { weatherData, isLoading, getNewWeatherData } = useAxiosFetch();
+  const [fetchedData, setFetchedData] = useState(weatherData);
 
   const handleInput = (e) => {
     if (e.key === 'Enter') {
@@ -13,37 +14,51 @@ function App() {
   };
 
   useEffect(() => {
-    console.log(weatherData);
+    setFetchedData(weatherData);
   }, [weatherData]);
+
+  if (!fetchedData || fetchedData.length === 0 || isLoading) {
+    return <div>LOADING</div>;
+  }
 
   return (
     <div className="wrapper">
-      {isLoading ? <h1>yes</h1> : <h1>no</h1>}
       <input type="text" placeholder="type" onKeyDown={handleInput} />
       <div className="app-top">
         <div className="app-text__fulldate">
           <Typography
             styleType={TYPOGRAPHY_STYLES.SUBCAPTION}
-            text="Monday, 27th April"
+            text={new Date().toDateString()}
           />
         </div>
         <div className="app-text__time">
-          <Typography styleType={TYPOGRAPHY_STYLES.CAPTION} text="6:27am" />
+          <Typography
+            styleType={TYPOGRAPHY_STYLES.CAPTION}
+            text={new Date().toLocaleTimeString()}
+          />
         </div>
         <div className="app-text__city">
-          <Typography styleType={TYPOGRAPHY_STYLES.SUBCAPTION} text="London" />
+          <Typography
+            styleType={TYPOGRAPHY_STYLES.SUBCAPTION}
+            text={fetchedData.main.temp}
+          />
         </div>
       </div>
       <div className="app-middle">
         <div className="app-text__fulldate">{/* weather icon */}</div>
         <div className="app-text__temp">
-          <Typography styleType={TYPOGRAPHY_STYLES.CAPTION} text="10*" />
+          <Typography
+            styleType={TYPOGRAPHY_STYLES.CAPTION}
+            text={fetchedData.main.temp}
+          />
         </div>
       </div>
       <div className="app-bottom">
-        <div className="app-text__date">{/* weather icon */}</div>
         <div className="app-text__temp">
-          <Typography styleType={TYPOGRAPHY_STYLES.CAPTION} text="10*" />
+          <Typography
+            styleType={TYPOGRAPHY_STYLES.CAPTION}
+            text={new Date().toDateString()}
+          />
         </div>
       </div>
     </div>
