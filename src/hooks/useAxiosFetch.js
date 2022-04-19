283330a93936a0db1ba87a7b2298991be4f35880
cipto.hartanto <cipto.hartanto@yahoo.com>
2022-04-19 22:43:34 +0800

    🩹 update useAxiosFetch

--- src/hooks/useAxiosFetch.js
+++ src/hooks/useAxiosFetch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const useAxiosFetch = () => {
-  const BASE_URL = `https://api.openweathermap.org/data/2.5/`;
+  const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?`;
 
   const [weatherData, setweatherData] = useState([]);
   const [fetchError, setFetchError] = useState(null);
@@ -12,7 +12,7 @@ const useAxiosFetch = () => {
   const getNewWeatherData = async (q) => {
     setIsLoading(true);
     try {
-      const { data } = await axios.get(`${BASE_URL}/weather?`, {
+      const { data } = await axios.get(`${BASE_URL}/`, {
         params: {
           q,
           appid: process.env.REACT_APP_WEATHER_KEY,
@@ -31,12 +31,10 @@ const useAxiosFetch = () => {
     if (isMounted) {
       getNewWeatherData('taipei');
     }
-
     const cleanup = () => {
       isMounted = false;
       source.cancel();
     };
-
     return () => cleanup;
   }, []);
 
