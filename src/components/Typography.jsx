7bc49f16f08aa41147f4a9b971f3723bdd42bf03
cipto.hartanto <cipto.hartanto@yahoo.com>
2022-04-23 20:51:17 +0800

    🎨 update Typography.jsx (more flexible)

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -1,39 +1,74 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 import '../styles/typography.css';
 
+// 1. h1, title -> 40px
+// 2. h2, subtitle -> 24px
+// 3. h3, caption -> 18px
+// 4. h4, subcaption -> 14px
+// 5. h5, text -> 12px
 export const TYPOGRAPHY_STYLES = {
-  SUBTITLE: 'subtitle',
-  TITLE: 'title',
-  CAPTION: 'caption',
-  SUBCAPTION: 'subcaption',
-  TEXT: 'text',
+  SUBTITLE: 'h1',
+  TITLE: 'h2',
+  CAPTION: 'h3',
+  SUBCAPTION: 'h4',
+  TEXT: 'h5',
 };
 
-const Typography = ({ styleType, text }) => {
-  // 1. h1, title -> 40px
-  // 2. h2, subtitle -> 24px
-  // 3. h3, caption -> 18px
-  // 4. h4, subcaption -> 14px
-  // 5. h5, text -> 12px
-  const typoUI = useMemo(() => {
+// 1. italic
+// 2. bold
+
+export const TYPOGRAPHY_ACCENTS = {
+  ITALIC: 'italic',
+  BOLD: 'bold',
+};
+
+const Typography = ({ styleType, text, accent }) => {
+  const [cssClasses, setCssClasses] = useState(['typography']);
+
+  const updateAccentCSS = () => {
+    if (!accent) return;
+    switch (accent) {
+      case TYPOGRAPHY_ACCENTS.BOLD:
+        setCssClasses(cssClasses.push('typography--bold'));
+        break;
+      case TYPOGRAPHY_ACCENTS.ITALIC:
+        setCssClasses(cssClasses.push('typography--italic'));
+        break;
+      default:
+    }
+  };
+
+  const updateStyleCSS = () => {
     switch (styleType) {
       case TYPOGRAPHY_STYLES.TITLE:
-        return <h1 className="typography typography--title">{text}</h1>;
-
+        setCssClasses(cssClasses.push('typography--title'));
+        break;
       case TYPOGRAPHY_STYLES.SUBTITLE:
-        return <h2 className="typography typography--subtitle">{text}</h2>;
+        setCssClasses(cssClasses.push('typography--subtitle'));
+        break;
 
       case TYPOGRAPHY_STYLES.CAPTION:
-        return <h3 className="typography typography--caption">{text}</h3>;
+        setCssClasses(cssClasses.push('typography--caption'));
+        break;
 
       case TYPOGRAPHY_STYLES.SUBCAPTION:
-        return <h4 className="typography typography--subcaption">{text}</h4>;
+        setCssClasses(cssClasses.push('typography--subcaption'));
+        break;
 
       default:
-        return <h5 className="typography typography--text">{text}</h5>;
+        setCssClasses(cssClasses.push('typography--text'));
     }
-  }, [styleType]);
+  };
+
+  const typoUI = useMemo(() => {
+    updateAccentCSS();
+    updateStyleCSS();
+
+    const TheTag = styleType;
+
+    return <TheTag className={cssClasses.join(' ')}>{text}</TheTag>;
+  }, []);
 
   return typoUI;
 };
